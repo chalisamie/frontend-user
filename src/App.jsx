4af261b36a022ddddd7cd02ae6e850ca2ee7ddd0
Chalisamie <memee.sks@gmail.com>
2024-12-02 16:00:06 +0700

    add data

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import SearchBar from "./components/SearchBar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Verify from "./pages/Verify";
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
   return (
